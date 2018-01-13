3917a06a94d363abdeddefb6d5c893df373fe7e4
Colin Chartier <colin.n.chartier@gmail.com>
2018-01-13 13:48:49 -0500

    Excellent clouds.

---
+++ Assets/Background/CloudBehavior.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudBehavior : MonoBehaviour {
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.velocity = new Vector2(Random.Range(0.1f, 1f), 0);
+        float scale = Random.Range(0.5f, 1.5f);
+        transform.localScale *= Random.Range(0.1f, 1f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Background/CloudSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudSpawner : MonoBehaviour
+{
+    public int counter = 0;
+    public GameObject cloud;
+    public int x = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter++;
+        if (counter == 120)
+        {
+            counter = 0;
+            Instantiate(cloud, new Vector3(-5, 0), new Quaternion(0, 0, 90, 0));
+            x++;
+        }
+    }
+}
