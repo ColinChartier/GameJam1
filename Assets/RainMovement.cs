5fa1df1bf0e0c55a154d91ee0c691835973d9c66
Filip Tomin <filip.tomin@mail.utoronto.ca>
2018-01-13 12:08:49 -0500

    Added rain movement

---
+++ Assets/RainMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainMovement : MonoBehaviour {
+
+    public float speed = 3;
+    public int score;
+    public Transform pos;
+
+	// Use this for initialization
+	void Start () {
+        //pos = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Get keys
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        pos.SetPositionAndRotation(new Vector3(pos.position.x + x * speed, pos.position.y + y * speed, pos.position.z), new Quaternion());
+	}
+}
