ca1ee8c359370dc0b3fb6d7f056c5e93a57b3b3e
doome1337 <dmitry.paramonov@gmail.com>
2018-01-13 12:07:09 -0500

    Beginning work on obstacles.

---
+++ Assets/obstacle_script.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class obstacle_script : MonoBehaviour {
+    bool attached = false;
+    public int size = 10;
+    public int value = 0;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+    }
+
+    void OnCollisionEnter2D (Collision2D collision) {
+        if (collision.gameObject.name != "Raindrop") {
+            return;
+        }
+        Debug.Log("Player has hit obstacle");
+        //if (collision.gameObject.score > size) {
+            Debug.Log("Player should absorb obstacle.");
+        //} else {
+            Debug.Log("Player should be damaged by obstacle.");
+        //}
+    }
+}
---
+++ Assets/spawner_script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner_script : MonoBehaviour {
+    public int counter = 0;
+    public GameObject obstacle;
+    public int x = 0;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        counter++;
+        if (counter == 120) {
+            counter = 0;
+            Instantiate(obstacle, new Vector3(x, 0), new Quaternion());
+            x++;
+        }
+    }
+}
